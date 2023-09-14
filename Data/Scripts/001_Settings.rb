106bb67f3b870cca186be7ca45b1e56ed5b38c47
abacaxi-queer <120669789+abacaxi-queer@users.noreply.github.com>
2023-09-14 11:48:02 -0300

    actually changed screen to 640 x 480

--- Data/Scripts/001_Settings.rb
+++ Data/Scripts/001_Settings.rb
@@ -353,9 +353,9 @@ module Settings
   #=============================================================================
 
   # The default screen width (at a scale of 1.0).
-  SCREEN_WIDTH  = 512
+  SCREEN_WIDTH  = 640
   # The default screen height (at a scale of 1.0).
-  SCREEN_HEIGHT = 384
+  SCREEN_HEIGHT = 480
   # The default screen scale factor. Possible values are 0.5, 1.0, 1.5 and 2.0.
   SCREEN_SCALE  = 1.0
 
